e82b6ae9decf914c73443c7e7d19589c7e076fbe
Willyam Santana Almeida <willyamalmeida@hotmail.com>
2018-02-02 17:32:22 -0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
     <h1>My example with vue-grid</h1>
+    <vue-grid :config="config"></vue-grid>
   </div>
 </template>
 
 <script>
+import VueGrid from "wa-vue-grid";
 
 export default {
-  name: 'App',
-}
-</script>
+    name: "App",
 
+    components: {
+        VueGrid
+    },
+
+    data() {
+        return {
+            config: {
+                collumns: [
+                    {name: 'Name'},
+                    {name: 'Email'}
+                ]
+            }
+        };
+    }
+};
+</script>
